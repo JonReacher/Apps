387b278126de59ec4734e01c44958679f7679e95
Jon <jon.m.ragland@gmail.com>
2023-04-06 19:04:52 -0400

    Create fireBaseconfig.js

---
+++ fireBaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXsDg39qSivf0FwGwcPejjDXoY8Z_Yp74",
+  authDomain: "brightfuture-a7247.firebaseapp.com",
+  projectId: "brightfuture-a7247",
+  storageBucket: "brightfuture-a7247.appspot.com",
+  messagingSenderId: "364124977462",
+  appId: "1:364124977462:web:886bb955090e5e44719b97",
+  measurementId: "G-0Z4GFMJSN6"
+};
